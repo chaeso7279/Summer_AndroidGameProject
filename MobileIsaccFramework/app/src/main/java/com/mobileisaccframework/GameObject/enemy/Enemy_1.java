83bc8ce9f1602b68eb968f335ae9cfac5e8108c1
sujin22 <aoghksj@naver.com>
2020-07-16 22:33:34 +0900

    Enemy_1, Enemy_2 클래스 추가

---
+++ MobileIsaccFramework/app/src/main/java/com/mobileisaccframework/GameObject/enemy/Enemy_1.java
@@ -0,0 +1,4 @@
+package com.mobileisaccframework.GameObject.enemy;
+
+public class Enemy_1 extends Enemy {
+}
---
+++ MobileIsaccFramework/app/src/main/java/com/mobileisaccframework/GameObject/enemy/Enemy_2.java
@@ -0,0 +1,4 @@
+package com.mobileisaccframework.GameObject.enemy;
+
+public class Enemy_2 extends Enemy {
+}
