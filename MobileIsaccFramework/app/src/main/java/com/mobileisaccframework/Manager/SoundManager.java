4da7b5a1eb288776858ab268356f9152aeb3c40c
chaeso7279 <chaeso7279@gmail.com>
2020-07-19 05:42:36 +0900

    사운드 매니저 수정전 커밋

--- MobileIsaccFramework/app/src/main/java/com/mobileisaccframework/Manager/SoundManager.java
+++ MobileIsaccFramework/app/src/main/java/com/mobileisaccframework/Manager/SoundManager.java
@@ -11,17 +11,11 @@ public class SoundManager {
     private SoundPool m_SoundPool;
     private HashMap m_SoundPoolMap;
     private AudioManager m_AudioManager;
-    private Context context;
-    private static SoundManager m_instance;
-
-    private static SoundManager getInstance(){
-        if(m_instance == null) m_instance = new SoundManager();
-        return m_instance;
-    }
+    private Context m_context;
 
     //초기화
     public void Initialize(Context _context){
-        this.context = context;
+        m_context = _context;
         m_SoundPool = new SoundPool(4, AudioManager.STREAM_MUSIC, 0);
         m_SoundPoolMap = new HashMap();
         m_AudioManager = (AudioManager)_context.getSystemService(Context.AUDIO_SERVICE);
@@ -29,23 +23,31 @@ public class SoundManager {
     }
 
     //음원을 추가하는 메소드
-    public void addSound(int _index, int _soundID){
-        int id = m_SoundPool.load(context, _soundID, 1); //사운드를 로드
+    public void AddSound(int _index, int _soundID){
+        int id = m_SoundPool.load(m_context, _soundID, 1); //사운드를 로드
         m_SoundPoolMap.put(_index, id); //해시맵에 아이디 값을 받아온 인덱스 저장
     }
 
     //음원 재생하는 메소드
-    public void play(int _index){
+    public void Play(int _index){
         float streamVolume = m_AudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
-        streamVolume = streamVolume/m_AudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        streamVolume = streamVolume / m_AudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
         m_SoundPool.play((Integer) m_SoundPoolMap.get(_index), streamVolume, streamVolume, 1, 0, 1f);
     }
 
     //음원 무한 재생하는 메소드
-    public void playLooped(int _index){
+    public void PlayLooped(int _index){
         float streamVolume = m_AudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
         streamVolume = streamVolume/m_AudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
         m_SoundPool.play((Integer) m_SoundPoolMap.get(_index), streamVolume, streamVolume, 1, -1, 1f);
+    }
+
+    // 싱글톤
+    private static SoundManager m_instance;
+    public static SoundManager getInstance() {
+        if(m_instance == null)
+            m_instance = new SoundManager();
+        return m_instance;
     }
 
 }
